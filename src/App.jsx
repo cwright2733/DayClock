fbc414e5deacd155a54f04da4d4f19fb0314a89f
Cameron Wright <arabia.cw@gmail.com>
2019-11-26 18:28:37 -0500

    world clock format

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import React, {Component} from 'react';
-
+import './App.css';
 
 
 class App extends Component{
     render(){
         return(
-            <div>
-                <div>Countdown to December 25, 2017</div>
+            <div className="App">
+                <div className="App-title">Countdown to December 25, 2017</div>
                 <div>
-                    <div>14 Days</div>
-                    <div>30 hours</div>
-                    <div>15 minutes</div>
-                    <div>20 Seconds</div>
+                    <div className="Clock-days">14 Days</div>
+                    <div className="Clock-hours">30 hours</div>
+                    <div className="Clock-minutes">15 minutes</div>
+                    <div className="Clock-seconds">20 Seconds</div>
                 </div>
+                <input placeholder='new date'/>
+                <button>Submit</button>
             </div>
         )
         
